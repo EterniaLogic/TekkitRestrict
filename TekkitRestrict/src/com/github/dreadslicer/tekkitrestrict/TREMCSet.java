9a183c2c22129aa71451c4863ad14a3ffae29d49
Taico Aerts <taicoaerts@hotmail.com>
2013-05-31 09:50:25 +0200

    Changed the way EMC is set. - An EMC value of 0 now actually removes the EMC value of that item. - Improvement to the way the strings in the config are handled. - "<id>[:<data>] <EMC>" where data can consist of multiple values separated by comma's. Ranges can be specified with <begin>-<end> - "10:2-4,12 100" Will set the EMC values of 10:2, 10:3, 10:4 and 10:12 to 100.

--- TekkitRestrict/src/com/github/dreadslicer/tekkitrestrict/TREMCSet.java
+++ TekkitRestrict/src/com/github/dreadslicer/tekkitrestrict/TREMCSet.java
@@ -1,32 +1,166 @@
 package com.github.dreadslicer.tekkitrestrict;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class TREMCSet {
-	private static List<String> ak;
+	private static List<String> configEMC;
 
 	public static void reload() {
-		ak = tekkitrestrict.config.getStringList("SetEMC");
-		setEMCs();
+		configEMC = tekkitrestrict.config.getStringList("SetEMC");
+		loadConfigEMC();
+	}
+	
+	/**
+	 * Add or set the EMC value of a single item.
+	 * If EMC = 0, it will remove the EMC value of that item.
+	 * @see #removeEMC(int, int)
+	 */
+	public static void setEMC(int id, int data, int EMC){
+		if (EMC == 0)
+			removeEMC(id, data);
+		else
+			ee.EEMaps.addEMC(id, data, EMC);
+	}
+	
+	/**
+	 * Remove the EMC value of an item.
+	 * WARNING: If that item is also used as fuel, unexpected behavior may occur.
+	 * */
+	public static void removeEMC(int id, int data){
+		HashMap<Integer, Integer> old = (HashMap<Integer, Integer>) ee.EEMaps.alchemicalValues.get(id);
+		if (old == null) return;
+		old.remove(data);
+		ee.EEMaps.alchemicalValues.put(id, old);
 	}
 
-	private static void setEMCs() {
-		if (tekkitrestrict.EEEnabled) {
-			for (int i = 0; i < ak.size(); i++) {
-				String ic = ak.get(i);
-				if (ic.contains(" ")) {
-					String[] pc = ic.split(" ");
-					String emc = pc[1];
-					int EMC = Integer.valueOf(emc);
-					List<TRCacheItem> iss = TRCacheItem.processItemString("",
-							pc[0], -1);
-					for (TRCacheItem cr : iss) {
-						int id = cr.id;
-						int data = cr.getData() == -10 ? 0 : cr.getData();
-						ee.EEMaps.addEMC(id, data, EMC);
+	private static void loadConfigEMC() {
+		if (!tekkitrestrict.EEEnabled) return;
+		
+		for (String current : configEMC){
+			if (!current.contains(" ")){
+				Log.Config.Warning("You have an invalid value in SetEMC in ModModifications.config: \""+current+"\"");
+				continue;
+			}
+			
+			String values[] = current.split(" ");
+			int EMC = 0, id = 0;
+			ArrayList<Integer> data;
+			
+			try {
+				EMC = Integer.parseInt(values[1]);
+			} catch (NumberFormatException ex){
+				Log.Config.Warning("You have an invalid value in SetEMC in ModModifications.config: \""+current+"\"");
+				continue;
+			}
+			
+			if (values[0].contains(":")){
+				String temp[] = values[0].split(":");
+				
+				data = handleData(temp[1], current);
+				
+				try {
+					id = Integer.parseInt(temp[0]);
+				} catch (NumberFormatException ex){
+					Log.Config.Warning("You have an invalid value in SetEMC in ModModifications.config: \""+current+"\"");
+					continue;
+				}
+			} else {
+				data = new ArrayList<Integer>();
+				data.add(0);
+				
+				try {
+					id = Integer.parseInt(values[0]);
+				} catch (NumberFormatException ex){
+					Log.Config.Warning("You have an invalid value in SetEMC in ModModifications.config: \""+current+"\"");
+					continue;
+				}
+			}
+			
+			for (int currentdata : data){
+				setEMC(id, currentdata, EMC);
+			}
+		}
+		
+		/*for (int i = 0; i < configEMC.size(); i++) {
+			String ic = configEMC.get(i);
+			if (ic.contains(" ")) {
+				String[] pc = ic.split(" ");
+				String emc = pc[1];
+				int EMC = Integer.valueOf(emc);
+				List<TRCacheItem> iss = TRCacheItem.processItemString("",
+						pc[0], -1);
+				for (TRCacheItem cr : iss) {
+					int id = cr.id;
+					int data = cr.getData() == -10 ? 0 : cr.getData();
+					ee.EEMaps.addEMC(id, data, EMC);
+				}
+			}
+		}*/
+	}
+	
+	/**
+	 * Get a list of Integers from a String range.
+	 * @return An ArrayList of Integers with the values specified by the given range.
+	 */
+	private static ArrayList<Integer> handleData(String data, String key){
+		ArrayList<Integer> allData = new ArrayList<Integer>();
+		if (data == null){
+			allData.add(0);
+		}
+		else if (data.equals("*")) {
+			Log.Config.Notice("Usage of the * datavalue for setting EMC is highly discouraged! It will increase the loadtime (and possibly also lagg) tremendeously. Please use ranges instead.");
+			for (int i = 0;i<=65535;i++){
+				allData.add(i);
+			}
+		}
+		else if (data.contains(",")){
+			String temp[] = data.split(",");
+			for (String current : temp){
+				if (current.contains("-")){
+					String temp2[] = current.split("-");
+					if (!temp2[0].matches("\\d+") || !temp2[1].matches("\\d+")){
+						Log.Config.Warning("The data value of '"+key+"' in SetEMC (ModModifications.config) is invalid!");
+						allData.clear();
+						allData.add(0);
+					} else {
+						int end = Integer.parseInt(temp2[1]);
+						for (int i = Integer.parseInt(temp2[0]); i<=end; i++){
+							allData.add(i);
+						}
+					}
+				} else {
+					if (!current.matches("\\d+")){
+						Log.Config.Warning("The data value of '"+key+"' in SetEMC (ModModifications.config) is invalid!");
+						allData.clear();
+						allData.add(0);
+					} else {
+						allData.add(Integer.parseInt(current));
 					}
 				}
 			}
+		} else if (data.contains("-")){//No comma's, only a - so just 1 split needed.
+			String temp[] = data.split("-");
+			if (!temp[0].matches("\\d+") || !temp[1].matches("\\d+")){
+				Log.Config.Warning("The data value of '"+key+"' in SetEMC (ModModifications.config) is invalid!");
+				allData.clear();
+				allData.add(0);
+			} else {
+				int end = Integer.parseInt(temp[1]);
+				for (int i = Integer.parseInt(temp[0]); i<=end; i++){
+					allData.add(i);
+				}
+			}
+		} else { //Only 1 value. 
+			if (!data.matches("\\d+")){
+				Log.Config.Warning("The data value of '"+key+"' in SetEMC (ModModifications.config) is invalid!");
+				allData.clear();
+				allData.add(0);
+			} else {
+				allData.add(Integer.parseInt(data));
+			}
 		}
+		return allData;
 	}
 }
