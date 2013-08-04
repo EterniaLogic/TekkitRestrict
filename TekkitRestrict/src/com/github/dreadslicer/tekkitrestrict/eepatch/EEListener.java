4aa0c2dfd61a8aa587e577c5bd499aa04dfc11b2
Taeir <info@taico.nl>
2013-08-04 22:29:30 +0200

    Small Build Patch changes

--- TekkitRestrict/src/com/github/dreadslicer/tekkitrestrict/eepatch/EEListener.java
+++ TekkitRestrict/src/com/github/dreadslicer/tekkitrestrict/eepatch/EEListener.java
@@ -1,16 +1,14 @@
 package com.github.dreadslicer.tekkitrestrict.eepatch;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import ee.events.EEEnums.EEAction2;
 import ee.events.EEToolEvent;
 
 public class EEListener implements Listener {
 	@EventHandler
 	public void onToolEvent(EEToolEvent event){
-		Player player = event.getPlayer();
-		EEAction2 action = event.getExtraInfo();
+		//Player player = event.getPlayer();
+		//EEAction2 action = event.getExtraInfo();
 	}
 }
